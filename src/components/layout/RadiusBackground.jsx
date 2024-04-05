57bc4627e81256e1bb6eebd3d2da2809a2dbae5f
gabriel <vnvsgabe@gmail.com>
2024-04-05 20:36:53 -0300

    criado RadiusBackground

---
+++ src/components/layout/RadiusBackground.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+import styled from "styled-components";
+
+const RadiusContainer = styled.View`
+  border-top-right-radius: 100px;
+  border-top-left-radius: 100px;
+  background-color: aliceblue;
+  width: 100%;
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+`;
+
+export default function RadiusBackground({ children }) {
+  return <RadiusContainer>{children}</RadiusContainer>;
+}
