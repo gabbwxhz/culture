0cf78ebc34e2d18a9933ff4dabd9761fb644f0e6
gabriel <vnvsgabe@gmail.com>
2024-04-06 16:59:58 -0300

    adicionado rotas de signups

--- App.jsx
+++ App.jsx
@@ -5,8 +5,10 @@ import { createStackNavigator } from "@react-navigation/stack";
 
 import LoginScreen from "./src/screens/LoginScreen";
 import SignupChoose from "./src/screens/signup/SignupChoose";
-import ExpositorSignup from "./src/screens/signup/ExpositorSignup";
+import ExpositorSignup from "./src/screens/signup/expositor-signup/ExpositorSignup";
 import VisitorSignup from "./src/screens/signup/VisitorSignup";
+import LegalExpositorUserSignup from "./src/screens/signup/expositor-signup/LegalExpositorUserSignnup";
+import ExpositorUserSignup from "./src/screens/signup/expositor-signup/ExpositorUserSignup";
 
 const Stack = createStackNavigator();
 
@@ -32,6 +34,16 @@ export default function App() {
         <Stack.Screen
           name="VisitorSignup"
           component={VisitorSignup}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="ExpositorUserSignup"
+          component={ExpositorUserSignup}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="LegalExpositorUserSignup"
+          component={LegalExpositorUserSignup}
           options={{ headerShown: false }}
         />
       </Stack.Navigator>
