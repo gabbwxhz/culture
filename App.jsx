c65f82f7b91bf4833f8b8c056d1114c7f4eb1cfb
gabriel <vnvsgabe@gmail.com>
2024-04-06 16:01:13 -0300

    ajustado rotas

--- App.jsx
+++ App.jsx
@@ -4,6 +4,9 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 
 import LoginScreen from "./src/screens/LoginScreen";
+import SignupChoose from "./src/screens/signup/SignupChoose";
+import ExpositorSignup from "./src/screens/signup/ExpositorSignup";
+import VisitorSignup from "./src/screens/signup/VisitorSignup";
 
 const Stack = createStackNavigator();
 
@@ -14,6 +17,21 @@ export default function App() {
         <Stack.Screen
           name="Login"
           component={LoginScreen}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="SignupChoose"
+          component={SignupChoose}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="ExpositorSignup"
+          component={ExpositorSignup}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="VisitorSignup"
+          component={VisitorSignup}
           options={{ headerShown: false }}
         />
       </Stack.Navigator>
