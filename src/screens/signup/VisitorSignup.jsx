f13f82ba1c2b6579144a8d61a771de0d01988394
gabriel <vnvsgabe@gmail.com>
2024-04-06 19:03:42 -0300

    iniciado construçao da tela de login do visitante

--- src/screens/signup/VisitorSignup.jsx
+++ src/screens/signup/VisitorSignup.jsx
@@ -1,10 +1,25 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { Image } from "react-native";
 
-export default function VisitorSignup() {
+import styled from "styled-components";
+
+import GradientBackground from "../../components/layout/GradientBackground";
+import RadiusBackground from "../../components/layout/RadiusBackground";
+import Title from "../../components/tipography/Title";
+import Card from "../../components/layout/Card";
+
+const StyledSafeAreaView = styled.SafeAreaView`
+  flex: 1;
+  background-color: aliceblue;
+  align-items: center;
+`;
+
+export default function SignupChoose({ navigation }) {
   return (
-    <View>
-      <Text>visitor</Text>
-    </View>
+    <StyledSafeAreaView>
+      <GradientBackground paddingTop="80px">
+        <RadiusBackground></RadiusBackground>
+      </GradientBackground>
+    </StyledSafeAreaView>
   );
 }
