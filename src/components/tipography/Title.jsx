412dcae853e8e141d3340451d658f5b9562b2595
gabriel <vnvsgabe@gmail.com>
2024-04-06 15:58:48 -0300

    adicionado props para padding em Title

--- src/components/tipography/Title.jsx
+++ src/components/tipography/Title.jsx
@@ -1,15 +1,19 @@
 import React from "react";
-
 import styled from "styled-components";
 
 const StyledTitle = styled.Text`
   font-size: 50px;
   font-weight: 900;
   text-align: center;
-  padding: 40px 0;
+  padding-top: ${(props) => props.paddingTop || "40px"};
+  padding-bottom: ${(props) => props.paddingBottom || "40px"};
   color: aliceblue;
 `;
 
-export default function Title({ children }) {
-  return <StyledTitle>{children}</StyledTitle>;
+export default function Title({ children, paddingTop, paddingBottom }) {
+  return (
+    <StyledTitle paddingTop={paddingTop} paddingBottom={paddingBottom}>
+      {children}
+    </StyledTitle>
+  );
 }
