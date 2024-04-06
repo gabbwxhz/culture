bdc41f820068b20721879455378ecf6c6bf45c43
gabriel <vnvsgabe@gmail.com>
2024-04-06 15:57:28 -0300

    criado tela de escolha para cadastro

--- src/screens/signup/SignupChoose.jsx
+++ src/screens/signup/SignupChoose.jsx
@@ -1,1 +1,54 @@
 import React from "react";
+import { Image } from "react-native";
+
+import styled from "styled-components";
+
+import GradientBackground from "../../components/layout/GradientBackground";
+import RadiusBackground from "../../components/layout/RadiusBackground";
+import Title from "../../components/tipography/Title";
+import Card from "../../components/layout/Card";
+
+const StyledSafeAreaView = styled.SafeAreaView`
+  flex: 1;
+  background-color: aliceblue;
+  align-items: center;
+`;
+
+const StyledText = styled.Text`
+  margin-top: 5px;
+  margin-bottom: ${(props) =>
+    props.marginBottom ? props.marginBottom : "70px"};
+  font-size: 20px;
+  color: #8b8b8b;
+  font-weight: 500;
+`;
+
+export default function SignupChoose({ navigation }) {
+  return (
+    <StyledSafeAreaView>
+      <GradientBackground>
+        <Title paddingBottom="70px" paddingTop="70px">
+          Cadastro
+        </Title>
+        <RadiusBackground>
+          <Card onPress={() => navigation.navigate("ExpositorSignup")}>
+            <Image
+              source={require("../../../assets/images/speaker.png")}
+              resizeMode="contain"
+              style={{ width: 150 }}
+            />
+          </Card>
+          <StyledText>Sou um expositor</StyledText>
+          <Card onPress={() => navigation.navigate("VisitorSignup")}>
+            <Image
+              source={require("../../../assets/images/user.png")}
+              resizeMode="contain"
+              style={{ width: 130 }}
+            />
+          </Card>
+          <StyledText marginBottom="10px">Sou um expositor</StyledText>
+        </RadiusBackground>
+      </GradientBackground>
+    </StyledSafeAreaView>
+  );
+}
