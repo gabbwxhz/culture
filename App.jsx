f67f4779ed6eefef01ebc6f8b3751dd3c1baab21
gabriel <vnvsgabe@gmail.com>
2024-04-06 14:32:57 -0300

    implementado rota inicial

--- App.jsx
+++ App.jsx
@@ -1,8 +1,22 @@
 import React from "react";
 
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+
 import LoginScreen from "./src/screens/LoginScreen";
 
+const Stack = createStackNavigator();
 
 export default function App() {
-  return <LoginScreen />;
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Login"
+          component={LoginScreen}
+          options={{ headerShown: false }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
