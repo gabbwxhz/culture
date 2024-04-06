e9e0fa531d668cac4ac2719708329a05a68172c8
gabriel <vnvsgabe@gmail.com>
2024-04-06 20:23:05 -0300

    estilizado signup visitante

--- src/screens/signup/VisitorSignup.jsx
+++ src/screens/signup/VisitorSignup.jsx
@@ -1,12 +1,13 @@
+// SignupChoose.js
 import React from "react";
-import { Image } from "react-native";
-
+import { ScrollView } from "react-native";
 import styled from "styled-components";
 
 import GradientBackground from "../../components/layout/GradientBackground";
 import RadiusBackground from "../../components/layout/RadiusBackground";
 import Title from "../../components/tipography/Title";
-import Card from "../../components/layout/Card";
+import Input from '../../components/inputs/Input';
+import Button from '../../components/inputs/Button';
 
 const StyledSafeAreaView = styled.SafeAreaView`
   flex: 1;
@@ -17,8 +18,25 @@ const StyledSafeAreaView = styled.SafeAreaView`
 export default function SignupChoose({ navigation }) {
   return (
     <StyledSafeAreaView>
-      <GradientBackground paddingTop="80px">
-        <RadiusBackground></RadiusBackground>
+      <GradientBackground paddingTop="60px">
+        <Title paddingBottom="80px">
+          Usuário Visitante
+        </Title>
+        <RadiusBackground>
+          <ScrollView
+            contentContainerStyle={{
+              justifyContent: "center",
+              alignItems: "center",
+              marginTop: 130,
+            }}
+          >
+            <Input placeholder={"Nome"} icon="person-outline" autoCorrect={false} />
+            <Input placeholder={"E-mail"} keyboardType="email-address" icon="mail-outline" autoCorrect={false} />
+            <Input placeholder={"Senha"} secureTextEntry icon="lock-outline" autoCorrect={false} />
+
+            <Button marginTop="40px">Cadastrar</Button>
+          </ScrollView>
+        </RadiusBackground>
       </GradientBackground>
     </StyledSafeAreaView>
   );
