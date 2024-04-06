6be66d7760393785c99ccc2d5478ec9d4a06269d
gabriel <vnvsgabe@gmail.com>
2024-04-06 19:06:51 -0300

    adicionado props para margin-bottom

--- src/components/tipography/Subtitle.jsx
+++ src/components/tipography/Subtitle.jsx
@@ -4,12 +4,13 @@ import styled from "styled-components";
 
 const StyledSubtitle = styled.Text`
   font-size: 26px;
-  margin-bottom: 20px;
+  margin-bottom: ${(props) => (props.marginBottom ? props.marginBottom : "20px") };
   font-weight: 700;
   text-align: center;
   color: #161616;
+  max-width: 250px;
 `;
 
-export default function Subtitle({ children }) {
-  return <StyledSubtitle>{children}</StyledSubtitle>;
+export default function Subtitle({ children, marginBottom }) {
+  return <StyledSubtitle marginBottom={marginBottom}>{children}</StyledSubtitle>;
 }
