4467c45547eaee73c29f7cafbc245b99f3bba291
gabriel <vnvsgabe@gmail.com>
2024-04-06 22:21:13 -0300

    build teste

--- App.jsx
+++ App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
+import { StatusBar} from "expo-status-bar";
 
 import LoginScreen from "./src/screens/LoginScreen";
 import SignupChoose from "./src/screens/signup/SignupChoose";
@@ -53,6 +54,7 @@ export default function App() {
           options={{ headerShown: false }}
         />
       </Stack.Navigator>
+      <StatusBar style="light" />
     </NavigationContainer>
   );
 }
